3cbcf8ed2094d55d19a1cfe54e12bb58fb702242
Preston Kemp <preston@prestonkemp.com>
2022-05-30 17:29:43 -0400

    update models

--- models.go
+++ models.go
@@ -48,3 +48,30 @@ type MyFMHUser struct {
 	StripeAccountID string
 	LocalSlackID    string
 }
+
+type QBAccount struct {
+	Name                          string  `json:"Name"`
+	SubAccount                    bool    `json:"SubAccount"`
+	FullyQualifiedName            string  `json:"FullyQualifiedName"`
+	Active                        bool    `json:"Active"`
+	Classification                string  `json:"Classification"`
+	AccountType                   string  `json:"AccountType"`
+	AccountSubType                string  `json:"AccountSubType"`
+	CurrentBalance                float64 `json:"CurrentBalance"`
+	CurrentBalanceWithSubAccounts float64 `json:"CurrentBalanceWithSubAccounts"`
+	CurrencyRef                   struct {
+		Value string `json:"value"`
+		Name  string `json:"name"`
+	} `json:"CurrencyRef"`
+	Domain    string `json:"domain"`
+	Sparse    bool   `json:"sparse"`
+	ID        string `json:"Id"`
+	SyncToken string `json:"SyncToken"`
+	MetaData  struct {
+		CreateTime      string `json:"CreateTime"`
+		LastUpdatedTime string `json:"LastUpdatedTime"`
+	} `json:"MetaData"`
+	ParentRef struct {
+		Value string `json:"value"`
+	} `json:"ParentRef,omitempty"`
+}
